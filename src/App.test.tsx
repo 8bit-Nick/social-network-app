0dffa7989921a6351400e909553ae47766787b28
8bit-Nick <geekneek28@gmail.com>
2021-11-29 11:40:34 +0200

    Add state.ts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {state} from "./redux/state";
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App contactsData={state.contactsData}
+              messagesData={state.messagesData}
+              postsData={state.postsData}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -3,36 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
+import {state} from "./redux/state";
 
-let contactsData = [
-  {id: 1, userName: 'Tommy'},
-  {id: 2, userName: 'John Wick'},
-  {id: 3, userName: 'Izabella'},
-  {id: 4, userName: 'Garold'},
-  {id: 5, userName: 'Hillary'},
-  {id: 6, userName: 'Antoine'},
-  {id: 7, userName: 'Ludwig'},
-  {id: 8, userName: 'Anna Lee'},
-];
 
-let messagesData = [
-  {id: 1, message: 'Hello!'},
-  {id: 2, message: 'How are you?'},
-  {id: 3, message: 'Lets go!'},
-  {id: 4, message: 'Nice to meet you.'},
-];
-
-let postsData = [
-  {id: 1, post: 'This is my first post!', likes: 15},
-  {id: 2, post: 'What music do you listen?', likes: 21},
-  {id: 3, post: 'Yeah, buddy!', likes: 370},
-  {id: 4, post: 'No, thanks.', likes: 3},
-];
 
 ReactDOM.render(
-  <App contactsData={contactsData}
-       messagesData={messagesData}
-       postsData={postsData}/>,
+  <App contactsData={state.contactsData}
+       messagesData={state.messagesData}
+       postsData={state.postsData}/>,
   document.getElementById('root')
 );
 
