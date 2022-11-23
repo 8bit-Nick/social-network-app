f1d4554c0fe3fab5720762085e4c9ce0b7e0301a
8bit-Nick <geekneek28@gmail.com>
2022-11-23 11:52:14 +0200

    dialogsReducer unit testing

---
+++ src/redux/tests/dialogsReducer.test.ts
@@ -0,0 +1,41 @@
+import dialogsReducer, { addNewMessage, DialogsType } from '../dialogsReducer';
+
+let state: DialogsType;
+beforeEach(() => {
+  state = {
+    contactsData: [
+      { id: 1, userName: 'Tommy' },
+      { id: 2, userName: 'John Wick' },
+      { id: 3, userName: 'Izabella' },
+      { id: 4, userName: 'Garold' },
+      { id: 5, userName: 'Hillary' },
+      { id: 6, userName: 'Antoine' },
+      { id: 7, userName: 'Ludwig' },
+      { id: 8, userName: 'Anna Lee' },
+    ],
+    messagesData: [
+      { id: 1, message: 'Hello!' },
+      { id: 2, message: 'How are you?' },
+      { id: 3, message: 'Lets go!' },
+      { id: 4, message: 'Nice to meet you.' },
+    ],
+  };
+});
+
+describe('addNewMessage testing', () => {
+  test('After adding a new message, the length of the array should be 5', () => {
+    const newState = dialogsReducer(
+      state,
+      addNewMessage('the first test message')
+    );
+    expect(newState.messagesData.length).toBe(5);
+  });
+
+  test('The new message should be "second test message"', () => {
+    const newState = dialogsReducer(
+      state,
+      addNewMessage('the second test message')
+    );
+    expect(newState.messagesData[4].message).toBe('the second test message');
+  });
+});
